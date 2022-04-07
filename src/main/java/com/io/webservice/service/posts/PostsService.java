371cb794ebf1e215d62d5ae33418c5eb4a3a3e0d
Gosongsu <ghzm0264@gmail.com>
2022-04-07 23:14:46 +0900

    PostsApiController, PostsSaveRequestDto, PostsService 추가

---
+++ src/main/java/com/io/webservice/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.io.webservice.service.posts;
+
+import com.io.webservice.domain.posts.PostsRepository;
+import com.io.webservice.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
